ed7c794e2a451d29c9ed4e160ca73886cd38ee94
Crazyplopl <crazyplopl17@gmail.com>
2023-01-26 21:27:03 +0000

    Update basicCalc.py
    added factorial and multiplication

--- Packages/basicCalc.py
+++ Packages/basicCalc.py
@@ -1,3 +1,5 @@
+from Packages.inputs import check
+import math
 def add(*numbers):
     valid = True
     for number in numbers:
@@ -5,3 +7,19 @@ def add(*numbers):
             number, valid = check.decimal(number)
     return sum(numbers)
 
+def multiply(*numbers):
+    valid = True
+    for number in numbers:
+        if valid == False:
+            number, valid = check.decimal(number)
+    return math.prod(numbers)
+
+def factorial(number):
+    number, valid = check.integer(number)
+    if valid:
+        return factorialFunc(number)
+
+def factorialFunc(number):
+    if number == 1:
+        return number
+    return number * factorialFunc(number-1)
