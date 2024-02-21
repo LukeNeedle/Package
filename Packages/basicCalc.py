32196acb0903b4fa821d640b15ccec8f93400a6a
Luke Needle <112342734+LukeNeedle@users.noreply.github.com>
2024-02-21 20:22:45 +0000

    basicCalc documentation

--- Packages/basicCalc.py
+++ Packages/basicCalc.py
@@ -2,6 +2,14 @@ from Packages.inputs import check
 import math
 def add(*numbers):
     valid = True
+    """
+    Adds together all parameters if they are valid.
+
+    Returns:
+        float: The sum of the parameters. None if any of the parameters aren't floats/integers.
+    """
+    result = 0
+    
     for number in numbers:
         if valid == False:
             number, valid = check.decimal(number)
@@ -9,6 +17,14 @@ def add(*numbers):
 
 def multiply(*numbers):
     valid = True
+    """
+    Multiplies all parameters together if they are valid.
+
+    Returns:
+        float: The result of the calculation. None if any of the parameters aren't floats/integers.
+    """
+    result = 0
+    
     for number in numbers:
         if valid == False:
             number, valid = check.decimal(number)
@@ -18,8 +34,26 @@ def factorial(number):
     number, valid = check.integer(number)
     if valid:
         return factorialFunc(number)
+    """
+    Calculates the factorial of the integer supplied.
+
+    Args:
+        number (integer): The number to find the factorial of.
+
+    Returns:
+        integer: The factorial of parapeter number. None if the parameter isn't an integers.
+    """
 
 def factorialFunc(number):
+    """
+    The backbone for the factorial function. For parameter checking please use .factorial instead of .factorialFunc.
+
+    Args:
+        number (integer): The number to find the factorial of.
+
+    Returns:
+        integer: The factorial of parapeter number.
+    """
     if number == 1:
         return number
     return number * factorialFunc(number-1)
